6d66258cf8ce255a7f67b32ec27284ef6b8e626b
van Heek <van_heek@gmx.de>
2025-04-15 15:54:05 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('osterjagd-v1').then(cache =>
-    cache.addAll(['./', './index.html', './scripts/game.js', './style.css', './manifest.json'])
-  ));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
